81481267a12d5dfbae6389b7adee047de76c8f8a
Azer444 <Azerrh@code.edu.az>
2022-06-29 19:20:51 +0400

    https://github.com/Azer444/Methods-Overriding-Overloading.gi

---
+++ Methods, Overriding, Overloading/Methods, Overriding, Overloading/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace Methods__Overriding__Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //3 ve 7 bolunen ededler (methodla)
+            //     Plus();
+            // }
+            //static void Plus()
+            // {
+            //     int n = 21;
+            //         if (n%3==0&&n%7==0)
+            //     {
+            //         Console.WriteLine("True");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("false");
+            //     }
+
+
+
+
+
+            //cut ededlerin cemi (methodla)
+
+            //Pluss();
+            //static void Pluss()
+            //{
+            //    int n = 12;
+            //    int m = 18;
+            //    int sum = 0;
+            //    if (n % 2 == 0 && m % 2 == 0)
+            //    {
+            //        sum = n + m;
+
+            //    }
+            //    Console.WriteLine(sum);
+            //}
+
+
+
+
+            //Minus();
+            //static void Minus()
+            //{
+
+
+            //Aralarindaki tek ededlerin cemi
+
+            //    int n = 2;
+            //    int m = 10;
+            //    int sum = 0;
+
+            //    for (int i = n; i < m; i++)
+            //    {
+            //        if (i % 2 == 1 && n < m)
+            //        {
+            //            sum += i;
+            //        }
+
+            //    }
+            //    Console.WriteLine(sum);
+            //}
+
+
+
+
+
+
+            //MinussAzer();
+
+            //static void MinussAzer()
+            //{
+            //    int[] numbers = { 1, 2,3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            //    int sum = 0;
+            //    for (int i = 0; i < numbers.Length; i++)
+            //    {
+            //        if (numbers[i] % 2 == 1)
+            //        {
+            //            sum += numbers[i];
+            //        }
+
+            //    }
+
+            //    Console.WriteLine(sum);
+            //}
+
+
+
+
+
+
+            //Array lerin cut olanlarin sayi
+
+            //ArrayPlus();
+            //static void ArrayPlus()
+            //{
+            //    int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            //    int count = 0;
+
+            //    for (int i = 0; i < number.Length; i++)
+            //    {
+            //        if (i%2==0)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    Console.WriteLine(count);
+            //}
+            
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        
+    }
+}
